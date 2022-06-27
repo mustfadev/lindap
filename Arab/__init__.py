c5d1d69c47bff129f49691cb15436b29a6a445f6
المطور مصطفى /dev Mustfa <74466394+mnxs8@users.noreply.github.com>
2022-06-27 20:26:49 +0300

    Update __init__.py

--- Arab/__init__.py
+++ Arab/__init__.py
@@ -6,16 +6,16 @@ from .core.session import iqthon
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 __version__ = "7.6"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "<t.me/iqthon>"
+__author__ = "<t.me/e1ggg>"
 __copyright__ = "telethon AR (C) 2020 - 2021  " + __author__
 iqthon.version = __version__
 iqthon.tgbot.version = __version__
-LOGS = logging.getLogger("IQTHON")
+LOGS = logging.getLogger("e1ggg")
 bot = iqthon
 StartTime = time.time()
 catversion = "7.6"
-if Config.UPSTREAM_REPO == "TelethonAr":
-    UPSTREAM_REPO_URL = "https://github.com/telethonArab/TelethonAr1"
+if Config.UPSTREAM_REPO == "linda":
+    UPSTREAM_REPO_URL = "https://github.com/mnxs8/TelethonAr1"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
